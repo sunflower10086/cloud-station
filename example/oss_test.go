c439b836a4edc7be9363023af2b5bc568fc2c393
zhao liu <lz18738377974@163.com>
2023-07-01 14:50:27 +0800

    添加viper读取env文件

--- example/oss_test.go
+++ example/oss_test.go
@@ -5,33 +5,49 @@ import (
 	"testing"
 
 	"github.com/aliyun/aliyun-oss-go-sdk/oss"
+	"github.com/spf13/viper"
 )
 
 var client *oss.Client
 
-//var (
-//	AccessKeyId     = os.Getenv("ALI_AK")
-//	AccessKeySecret = os.Getenv("ALI_SK")
-//	OssEndpoint     = os.Getenv("ALI_OSS_ENDPOINT")
-//	BucketName      = os.Getenv("ALI_BUCKET_NAME")
-//)
+var Conf = new(config)
 
-var (
-	AccessKeyId     = "LTAI5tFn7XFC9Lkv1vSJ55SA"
-	AccessKeySecret = "2YZknyvGJGCjaFA4lX9GdbnSmW4ZwY"
-	OssEndpoint     = "oss-cn-beijing.aliyuncs.com"
-	BucketName      = "lz-devcloud-station"
-)
+type config struct {
+	AccessKeyId     string `mapstructure:"ALI_AK"`
+	AccessKeySecret string `mapstructure:"ALI_SK"`
+	OssEndpoint     string `mapstructure:"ALI_OSS_ENDPOINT"`
+	BucketName      string `mapstructure:"ALI_BUCKET_NAME"`
+}
 
 // 测试oss的基本使用
 func init() {
+	ReadEnvFile()
+	c, err := oss.New(Conf.OssEndpoint, Conf.AccessKeyId, Conf.AccessKeySecret)
 
-	c, err := oss.New(OssEndpoint, AccessKeyId, AccessKeySecret)
-	fmt.Println(OssEndpoint)
 	if err != nil {
 		panic(err)
 	}
 	client = c
+}
+
+func ReadEnvFile() {
+	viper.SetConfigName("test")
+	viper.SetConfigType("env")
+
+	viper.AddConfigPath("../etc") // 查找配置文件所在的目录
+	if err := viper.ReadInConfig(); err != nil {
+		if _, ok := err.(viper.ConfigFileNotFoundError); ok {
+			// 配置文件未找到
+			fmt.Println("配置文件未找到")
+		} else {
+			// 其他错误
+			fmt.Println("加载配置文件错误")
+		}
+	}
+
+	if err := viper.Unmarshal(Conf); err != nil {
+		fmt.Println(err)
+	}
 }
 
 func TestBucketList(t *testing.T) {
