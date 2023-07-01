49b8827f47d95ae0965c6c8e61453cdea0010e49
zhao liu <lz18738377974@163.com>
2023-07-01 16:20:47 +0800

    添加viper读取env配置文件，防止自己的AccessKey泄露

--- main.go
+++ main.go
@@ -6,13 +6,16 @@ import (
 	"os"
 
 	"github.com/aliyun/aliyun-oss-go-sdk/oss"
+	"github.com/spf13/viper"
 )
+
+var Conf = new(config)
 
 var (
 	// 程序内置
-	accessKeyId     = "xx"
-	accessKeySecret = "xx"
-	ossEndpoint     = "oss-cn-beijing.aliyuncs.com"
+	accessKeyId     = Conf.AccessKeyId
+	accessKeySecret = Conf.AccessKeySecret
+	ossEndpoint     = Conf.OssEndpoint
 
 	// 默认配置
 	bucketName = "lz-devcloud-station"
@@ -22,6 +25,37 @@ var (
 
 	help = false
 )
+
+type config struct {
+	AccessKeyId     string `mapstructure:"ALI_AK"`
+	AccessKeySecret string `mapstructure:"ALI_SK"`
+	OssEndpoint     string `mapstructure:"ALI_OSS_ENDPOINT"`
+	BucketName      string `mapstructure:"ALI_BUCKET_NAME"`
+}
+
+func init() {
+	viper.SetConfigName("test")
+	viper.SetConfigType("env")
+
+	viper.AddConfigPath("./etc") // 查找配置文件所在的目录
+	if err := viper.ReadInConfig(); err != nil {
+		if _, ok := err.(viper.ConfigFileNotFoundError); ok {
+			// 配置文件未找到
+			fmt.Println("配置文件未找到")
+		} else {
+			// 其他错误
+			fmt.Println("加载配置文件错误")
+		}
+	}
+
+	if err := viper.Unmarshal(&Conf); err != nil {
+		fmt.Println(err)
+	}
+
+	accessKeyId = Conf.AccessKeyId
+	accessKeySecret = Conf.AccessKeySecret
+	ossEndpoint = Conf.OssEndpoint
+}
 
 func upLoad(filePath string) error {
 	// 1.初始化oss.client
@@ -37,7 +71,18 @@ func upLoad(filePath string) error {
 	}
 
 	// 3.上传文件
-	return bucket.PutObjectFromFile(filePath, filePath)
+	if err := bucket.PutObjectFromFile(filePath, filePath); err != nil {
+		return err
+	}
+
+	// 4. 打印下载链接
+	downloadUrl, err := bucket.SignURL(filePath, oss.HTTPGet, 60*60*24)
+	if err != nil {
+		return err
+	}
+	fmt.Printf("文件下载url %s\n", downloadUrl)
+	fmt.Println("下载链接有效期一天，请在一天内下载")
+	return nil
 }
 
 // 参数合法性校验
@@ -62,6 +107,7 @@ func loadParams() {
 	// 判断CLI 是否需要打印help信息
 	if help {
 		usage()
+		os.Exit(0)
 	}
 }
 
@@ -77,19 +123,18 @@ Options:
 }
 
 func main() {
-	// 参数校验
+	// 1.参数校验
 	loadParams()
-	// 参数检验
+	// 2.参数检验
 	if err := validate(); err != nil {
 		fmt.Printf("参数校验异常 %s\n", err.Error())
-		if !help {
-			usage()
-		}
 		return
 	}
 
+	// 3.上传文件
 	if err := upLoad(upLoadFile); err != nil {
 		fmt.Printf("上传文件错误 %s\n", err.Error())
+		return
 	}
 
 	fmt.Printf("文件 %s 上传成功 \n", upLoadFile)
