43c99d44b8f36c119793e9a350443ad5fa6af23d
zhao liu <lz18738377974@163.com>
2023-07-01 16:01:50 +0800

    补充 usage 打印提示信息

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"flag"
 	"fmt"
+	"os"
 
 	"github.com/aliyun/aliyun-oss-go-sdk/oss"
 )
@@ -18,6 +19,8 @@ var (
 
 	// 命令行上传
 	upLoadFile = ""
+
+	help = false
 )
 
 func upLoad(filePath string) error {
@@ -51,8 +54,26 @@ func validate() error {
 }
 
 func loadParams() {
+	flag.BoolVar(&help, "h", false, "打印帮助信息")
+
 	flag.StringVar(&upLoadFile, "f", "", "请输入文件的名称")
 	flag.Parse()
+
+	// 判断CLI 是否需要打印help信息
+	if help {
+		usage()
+	}
+}
+
+// 打印使用说明
+func usage() {
+	// 1.打印一些描述信息
+	fmt.Fprintf(os.Stderr, `cloud-station version: 0.0.1
+Usage: cloud-station [-h] -f <uplaod_file_path>
+Options:
+`)
+	// 2.打印有哪些参数可以使用，像-f
+	flag.PrintDefaults()
 }
 
 func main() {
@@ -61,6 +82,9 @@ func main() {
 	// 参数检验
 	if err := validate(); err != nil {
 		fmt.Printf("参数校验异常 %s\n", err.Error())
+		if !help {
+			usage()
+		}
 		return
 	}
 
